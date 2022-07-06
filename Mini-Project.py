b1c85df8e775abfd773f4440a08c54dc4da0c401
Darshangupta03 <98694439+Darshangupta03@users.noreply.github.com>
2022-07-06 22:51:42 +0530

    Create Mini-Project.py

---
+++ Mini-Project.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from tkinter import messagebox
+import random as r
+def button(frame):          
+    b=Button(frame,padx=1,bg="light blue",width=3,text="   ",font=('arial',60,'bold'),relief="sunken",bd=10)
+    return b
